a1546671120afe0609e2b6d059d2f0d65d29b768
chr005-dev <chr005@edu.zealand.dk>
2025-12-04 13:28:28 +0100

    Boat_9_story

--- HillerødSejlklub.Console/Program.cs
+++ HillerødSejlklub.Console/Program.cs
@@ -1,2 +1,8 @@
 ﻿using HillerødSejlklub.Library;
+Boat boat = new Boat(1);
+
+Booking booking = new Booking(boat,new DateTime(2025, 12, 23, 10, 0, 0),new DateTime(2025,12,23,16,0,0),"Bob",1,"The sea");
+booking.GetAll();
+Booking booking1 = new Booking(boat,new DateTime(2025, 12, 23, 16, 0, 0), new DateTime(2025, 12, 23, 18, 0, 0), "Bob", 1, "The sea");
+booking.GetAll();
 
