3ab9f2fd6a1601fd47fe51831be1f93c671334a1
Sebahattin Emre Türkoglu <set003@edu.zealand.dk>
2025-12-04 14:15:40 +0100

    Added Userrepository class, and added GetAllMembers method

--- HillerødSejlklub.Console/Program.cs
+++ HillerødSejlklub.Console/Program.cs
@@ -1,2 +1,5 @@
 ﻿using HillerødSejlklub.Library;
 
+User user = new User(1, "John Doe", "123 Main St", "555-1234", "hej123@gmail.com");
+
+
---
+++ HillerødSejlklub.Library/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HillerødSejlklub.Library
+{
+    public class UserRepository
+    {
+        private List<User> _users = new List<User>();
+
+        public void GetAllMembers()
+        {
+            foreach (var user in _users)
+            {
+               
+               Console.WriteLine($" Navn: {user.Name} -  ID: {user.Id} - Adresse: {user.Adress} - Telefon: {user.Phone} - Email: {user.Email}");
+            }
+        }
+
+     
+
+        public User GetUsersByName(string UserName)
+        {
+            foreach (User user in _users)
+            {
+                if (user.Name == UserName)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+
+      
+    }
+}
